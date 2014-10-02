aeabe6dbd787eb4510f447c366b6a370e73a8242
Dan <dan@debian>
2014-10-01 22:17:14 -0400

    Add more columns to the database schema Start the thread creation form

--- initdb.py
+++ initdb.py
@@ -21,13 +21,13 @@ def main():
     if len(sys.argv) > 1 and sys.argv[1] == 'd':
         # Entering some sample data
         b = Board('Random', 'b', True)
-        b_thread1 = Thread(b, datetime.datetime.now())
-        b_thread1_post1 = Post(b_thread1, 'First post content', None,
-                               datetime.datetime.now())
-        b_thread1_post2 = Post(b_thread1, 'Second post content', None,
-                               datetime.datetime.now())
-        b_thread1_post3 = Post(b_thread1, 'Third post content', None,
-                               datetime.datetime.now())
+        b_thread1 = Thread(b, 'A Subject', datetime.datetime.now())
+        b_thread1_post1 = Post(b_thread1, 'Person 1', 'First post content', None,
+                               'first@email.com', datetime.datetime.now())
+        b_thread1_post2 = Post(b_thread1, 'Person 2', 'Second post content', None,
+                               'second@email.com', datetime.datetime.now())
+        b_thread1_post3 = Post(b_thread1, 'Person 3', 'Third post content', None,
+                               'third@email.com', datetime.datetime.now())
         
         # The UndefinedVariable comments tell PyDev to suppress errors that it
         # wrongly generates for the add and commit methods.
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 
+# Flask imports
 from flask import Flask
+from flask import request
+from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 
+# Standard library imports
+import datetime
 import json
-from flask.templating import render_template
+import random
 
+# A few configuarion variables
 DBCREDS_FILENAME = 'dbcreds.json'
+DEFAULT_NAME = 'Anonymous'
+MAX_EMAIL_LENGTH = 30
+MAX_NAME_LENGTH = 30
+MAX_SUBJECT_LENGTH = 60
+FILE_DIRECTORY = 'images/'
+FILE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif']
 
 app = Flask(__name__)
 
@@ -50,12 +62,14 @@ class Thread(db.Model):
     __tablename__ = 'threads'
     id = db.Column(db.Integer, primary_key=True)
     board_id = db.Column(db.Integer, db.ForeignKey('boards.id'))
+    subject = db.Column(db.String(MAX_SUBJECT_LENGTH))
     timestamp = db.Column(db.DateTime)
     
     board = db.relationship('Board', backref='boards')
     
-    def __init__(self, board, timestamp):
+    def __init__(self, board, subject, timestamp):
         self.board = board
+        self.subject = subject
         self.timestamp = timestamp
 
 class Post(db.Model):
@@ -63,16 +77,20 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     thread_id = db.Column(db.Integer, db.ForeignKey('threads.id'),
                           primary_key=True)
+    name = db.Column(db.String(MAX_NAME_LENGTH))
     content = db.Column(db.String(10000))
     filename = db.Column(db.String(64))
+    email = db.Column(db.String(MAX_EMAIL_LENGTH))
     timestamp = db.Column(db.DateTime)
     
     thread = db.relationship('Thread', backref='posts')
     
-    def __init__(self, thread, content, filename, timestamp):
+    def __init__(self, thread, name, content, filename, email, timestamp):
         self.thread = thread
+        self.name = name
         self.content = content
         self.filename = filename
+        self.email = email
         self.timestamp = timestamp
 
 # Read-only URL trees
@@ -100,12 +118,43 @@ def thread(board_tag, thread):
 
 # Write-only URL trees
 
-@app.route('/submit/<board>/')
+@app.route('/submit/<board>/', methods=['POST'])
 def submit_thread(board):
-    return 'Submitting thread on board {}'.format(board)
+    # Check to make sure that all of the required fields are provided. Some of
+    # them can be absent, since they have default values. Also trim fields if
+    # they exceed their maximum length.
+    subject = request.form['subject'][:MAX_SUBJECT_LENGTH] \
+        if 'subject' in request.form else ''
+    email = request.form['email'][:MAX_EMAIL_LENGTH] \
+        if 'email' in request.form else ''
+    name = request.form['name'][:MAX_NAME_LENGTH] \
+        if 'name' in request.form else DEFAULT_NAME
+    
+    if 'message' not in request.form or \
+        len(request.form['message'].strip()) == 0:
+        return 'You must enter a message'
+    
+    timestamp = datetime.datetime.now()
+    
+    if 'upload' in request.files:
+        # Generate the files new name. For now, this will be the current time
+        # concatenated with a random number in a range big enough to avoid
+        # collision. The extension provided by the user is used, assuming it
+        # is an allowed extension.
+        provided_filename = request.files['upload'].filename
+        extension = provided_filename[provided_filename.rfind('.') + 1:].lower()
+        if extension not in FILE_EXTENSIONS:
+            return 'Extension {} is not allowed'.format(extension)
+        filename = '{}_{}.{}'.format( \
+           timestamp.strftime('%Y%m%d%H%M%S'),
+           random.getrandbits(32),
+           extension)
+        return filename
+    else:
+        return 'You must upload a file to start a thread.'
 
 @app.route('/submit/<board>/<thread>')
-def submit_post(board, thread):
+def submit_post(board, thread, methods=['POST']):
     return 'Submitting post to thread {} on board {}'.format(thread, board)
 
 @app.route('/')
